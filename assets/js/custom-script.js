be692d3a1bdfd3a076224f2d167996849e3f045b
sonawaneapekshit <apekshitsonawane@gmail.com>
2019-03-19 17:38:23 +0530

    Comment for down arrow animation added

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    // Animate on click of downarrow in landing page section of page
     $("a.downarrow").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
