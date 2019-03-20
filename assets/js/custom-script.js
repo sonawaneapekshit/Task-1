ee586fe27549919dcafd247642f1b987c309a808
sonawaneapekshit <apekshitsonawane@gmail.com>
2019-03-20 10:21:36 +0530

    Leaflet map code added

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    var mymap = L.map('map-container-google').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: '',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYXBla3NoaXRkdXJlIiwiYSI6ImNqczVvdThuNDBnaGU0NHBrc2NzZWtyeWgifQ.a8nDPGzTgd39TzKBsdbBEQ'
+    }).addTo(mymap);
+    //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    var popup = L.popup()
+        .setLatLng([51.5, -0.09])
+        .setContent("1234 First Street, Second County, Zip 12345, COUNTRY")
+        .openOn(mymap);
+
     // Animate on click of downarrow in landing page section of page
     $("a.downarrow").click(function(event) {
         event.preventDefault();
@@ -38,5 +51,4 @@ $(document).ready(function() {
             }
         });
     }).scroll();
-
 });
